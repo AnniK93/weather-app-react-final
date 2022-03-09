3e7b1bea80dbbdfd77dc6979b8584f267f0a95a9
Annika <85462906+AnniK93@users.noreply.github.com>
2022-03-09 14:12:47 +0000

    Added main and footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">Hello from Anni</header>
+      <main>Weather app goes here</main>
+      <footer>
+        <a
+          href="https://github.com/AnniK93/weather-app-react-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code by Anni
+        </a>
+      </footer>
     </div>
   );
 }
